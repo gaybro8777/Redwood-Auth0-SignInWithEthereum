99b2c15b99922f716c332e6d120fb45c9110c8d4
Rex Mok <Woodrex83>
2022-10-04 16:54:49 +0800

    Fix userMetadata

--- web/src/pages/AdminPage/AdminPage.js
+++ web/src/pages/AdminPage/AdminPage.js
@@ -8,7 +8,7 @@ const AdminPage = () => {
     <>
       <MetaTags title="Auth" description="Login page" />
 
-      <div className="max-w-md transition-all duration-300 mt-6 mx-auto w-full">
+      <div className="max-w-2xl transition-all duration-300 mt-6 mx-auto w-full">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:px-10">
           <div className="flex justify-center text-2xl font-medium max-h-8 max-w-sm mx-auto w-full h-full mb-3">
             <img src={logo} alt="Auth0" className="object-scale-down max-w-full max-h-full w-auto h-10 mx-auto" />
@@ -19,7 +19,9 @@ const AdminPage = () => {
               <div>
                 <h3>User Metadata</h3>
                 {userMetadata ? (
-                  <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
+                  <div className="overflow-hidden">
+                    {JSON.stringify(userMetadata, null, 2)}
+                  </div>
                 ) : (
                   "No user metadata defined"
                 )}
